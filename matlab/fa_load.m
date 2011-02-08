946685b5082d465fbe699ca2292612c1b1ea5485
mga83 <mga83@e099a375-04f9-0310-9d5f-a741eaff62e1>
2011-02-08 09:18:34 +0000

    FA viewer: Update fa_zoomer matlab scripts to use new fa-archiver
    git-svn-id: svn+ssh://serv0002.cs.diamond.ac.uk/home/subversion/repos/controls/diamond/trunk/python/fa-archiver@56975 e099a375-04f9-0310-9d5f-a741eaff62e1

--- matlab/fa_load.m
+++ matlab/fa_load.m
@@ -1,23 +1,21 @@
-% loadfa
-%   
-% Guenther Rehm
-% November 2010
+% fa_load
 %
 % Grab bpm data from the fa archiver for the last 36 hours or so.
 %
-% d = loadfa(tse, mask, type)
+% d = fa_load(tse, mask [, type [,server]])
 %
 % Input:
 %   tse = [start_time end_time]
 %   mask = vector containing bmps [1 2 3], or [78 79] etc.
 %   type = 'F' for full data
 %          'd' for 10072/64 decimated
-%          'D' for 10072/128^2 decimated
+%          'D' for 10072/16384 decimated
 %
 % Output:
 %   d = object containing all of the data
-function d = loadfa(tse, mask, type, server)
-fa_capture = '/home/mga83/epics/fa-archiver/build/fa-capture';
+function d = fa_load(tse, mask, type, server)
+fa_capture = 'fa-capture';
+% fa_capture = '/home/mga83/epics/fa-archiver/build/fa-capture';
 if nargin < 3
     type = 'F';
 end
@@ -30,8 +28,8 @@ end
 
 % Create temporary file to capture into
 [r, famat] = system('mktemp');
-if r ~= 0; error('no mktmp?'); end
-famat = famat(1:end-1);
+if r ~= 0; error('mktemp failed'); end
+famat = famat(1:end-1);     % Remove \n from returned filename
 
 % Use fa-capture utility to read required data into temporary file
 maskstr = sprintf('%d,', mask);
@@ -48,12 +46,12 @@ d = load('-mat', famat);
 delete(famat);
 
 % Compute the timestamps
-g = [d.gapix length(d.data)]+1;
+g = [d.gapix length(d.data)] + 1;
 for n = 1:(length(g)-1);
-    d.t(g(n):(g(n+1)-1)) = d.gaptimes(n)+(0:diff(g([n n+1]))-1)/d.f_s/3600/24;
+    d.t(g(n):g(n+1)-1) = d.gaptimes(n) + (0:diff(g([n n+1]))-1)/d.f_s/3600/24;
 end
 d.day = floor(d.t(1));
-d.t = d.t-d.day;
+d.t = d.t - d.day;
 
 
 function str = format_time(time)
@@ -63,7 +61,8 @@ format = 'yyyy-mm-ddTHH:MM:SS';
 str = datestr(time, format);
 % Work out the remaining unformatted fraction of a second
 delta_s = 3600 * 24 * (time - datenum(str, format));
-if 0 < delta_s && delta_s < 0.9999
+if delta_s > 0.9999; delta_s = 0.9999; end      % Fudge for last fraction
+if 0 < delta_s
     nano = sprintf('%.4f', delta_s);
     str = [str nano(2:end)];
 end
