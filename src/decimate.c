eed1fa18d04d10ee1063e11b66fa60dd2143dbb1
mga83 <mga83@e099a375-04f9-0310-9d5f-a741eaff62e1>
2011-04-28 09:47:37 +0000

    FA archiver: Convert CIC filter to use 64 bit intermediate values
    This increased the dynamic range to the point where we can safely use
    quite vigorous CIC filtering.
    
    git-svn-id: svn+ssh://serv0002.cs.diamond.ac.uk/home/subversion/repos/controls/diamond/trunk/python/fa-archiver@61592 e099a375-04f9-0310-9d5f-a741eaff62e1

--- src/decimate.c
+++ src/decimate.c
@@ -16,6 +16,13 @@
 #include "decimate.h"
 
 
+/* A couple of workspace declarations. */
+struct fa_entry_int64  { int64_t x, y; };
+struct fa_entry_double { double  x, y; };
+struct fa_row_int64  { struct fa_entry_int64  row[FA_ENTRY_COUNT]; };
+struct fa_row_double { struct fa_entry_double row[FA_ENTRY_COUNT]; };
+
+
 /* Incoming buffer of FA blocks and associated reader. */
 static struct reader_state *reader;
 static size_t fa_block_size;
@@ -50,11 +57,11 @@ static const struct config_entry config_table[] = {
 
 
 /* Workspace initialised from CIC configuration. */
-static struct fa_row *cic_accumulators;
-static struct fa_row **cic_histories;
+static struct fa_row_int64 *cic_accumulators;
+static struct fa_row_int64 **cic_histories;
 static int *cic_history_index;
 
-static struct fa_row *filter_buffer;
+static struct fa_row_int64 *filter_buffer;
 static double filter_scaling;
 
 
@@ -70,14 +77,17 @@ static bool initialise_configuration(void)
         return false;
 
     /* One accumulator for each stage. */
-    cic_accumulators = calloc(history_length.count, FA_FRAME_SIZE);
+    cic_accumulators = calloc(
+        history_length.count, sizeof(struct fa_row_int64));
     /* Array of history buffers for variable length differentiation stage. */
-    cic_histories = calloc(history_length.count, sizeof(struct fa_row *));
+    cic_histories = calloc(history_length.count, sizeof(struct fa_row_int64 *));
     for (int i = 0; i < history_length.count; i ++)
-        cic_histories[i] = calloc(history_length.data[i], FA_FRAME_SIZE);
+        cic_histories[i] = calloc(
+            history_length.data[i], sizeof(struct fa_row_int64));
     cic_history_index = calloc(history_length.count, sizeof(int));
     /* History buffer for compensation filter. */
-    filter_buffer = calloc(compensation_filter.count, FA_FRAME_SIZE);
+    filter_buffer = calloc(
+        compensation_filter.count, sizeof(struct fa_row_int64));
 
     /* Compute scaling factor for overall unit DC response for the entire filter
      * chain. */
@@ -102,37 +112,46 @@ static int decimation_counter;
 static int filter_index;
 
 
+/* Helper macro for repeated pattern in accumulate(). */
+#define ACCUMULATE_ROW(accumulator, row_in) \
+    do for (int i = 0; i < FA_ENTRY_COUNT; i ++) \
+    { \
+        (accumulator)->row[i].x += (row_in)->row[i].x; \
+        (accumulator)->row[i].y += (row_in)->row[i].y; \
+    } while(0)
+
 /* Accumulates a single update into the accumulator array for the integrating
  * part of the CIC filter, returns the last row. */
-static const struct fa_row * accumulate(const struct fa_row *row_in)
+static const struct fa_row_int64 * accumulate(const struct fa_row *row_in)
 {
-    for (int stage = 0; stage < history_length.count; stage ++)
+    /* The first stage converts 32 bit in into 64 bit intermediate results. */
+    ACCUMULATE_ROW(&cic_accumulators[0], row_in);
+    struct fa_row_int64 *last_row = &cic_accumulators[0];
+
+    /* The remaining rows are all uniform. */
+    for (int stage = 1; stage < history_length.count; stage ++)
     {
-        struct fa_row *accumulator = &cic_accumulators[stage];
-        for (int i = 0; i < FA_ENTRY_COUNT; i ++)
-        {
-            accumulator->row[i].x += row_in->row[i].x;
-            accumulator->row[i].y += row_in->row[i].y;
-        }
-        row_in = accumulator;
+        ACCUMULATE_ROW(&cic_accumulators[stage], last_row);
+        last_row = &cic_accumulators[stage];
     }
-    return row_in;
+    return last_row;
 }
 
 
 /* Performs repeated differentiation of raw decimated data. */
-static void differentiate(const struct fa_row *row_in, struct fa_row *row_out)
+static void differentiate(
+    const struct fa_row_int64 *row_in, struct fa_row_int64 *row_out)
 {
     for (int stage = 0; stage < history_length.count; stage ++)
     {
-        struct fa_row *history =
+        struct fa_row_int64 *history =
             &cic_histories[stage][cic_history_index[stage]];
         cic_history_index[stage] =
             (cic_history_index[stage] + 1) % history_length.data[stage];
 
         for (int i = 0; i < FA_ENTRY_COUNT; i ++)
         {
-            struct fa_entry in = row_in->row[i];
+            struct fa_entry_int64 in = row_in->row[i];
             row_out->row[i].x = in.x - history->row[i].x;
             row_out->row[i].y = in.y - history->row[i].y;
             history->row[i] = in;
@@ -149,24 +168,24 @@ static void filter_output(struct fa_row *row_out)
     if (filter_index >= compensation_filter.count)
         filter_index = 0;
 
-    double accumulator[FA_ENTRY_COUNT][2];
+    struct fa_row_double accumulator;
     memset(&accumulator, 0, sizeof(accumulator));
     for (int j = 0; j < compensation_filter.count; j ++)
     {
         double coeff = compensation_filter.data[j];
-        struct fa_row *row =
+        struct fa_row_int64 *row =
             &filter_buffer[(filter_index + j) % compensation_filter.count];
         for (int i = 0; i < FA_ENTRY_COUNT; i ++)
         {
-            accumulator[i][0] += coeff * row->row[i].x;
-            accumulator[i][1] += coeff * row->row[i].y;
+            accumulator.row[i].x += coeff * row->row[i].x;
+            accumulator.row[i].y += coeff * row->row[i].y;
         }
     }
 
     for (int i = 0; i < FA_ENTRY_COUNT; i ++)
     {
-        row_out->row[i].x = (int) (filter_scaling * accumulator[i][0]);
-        row_out->row[i].y = (int) (filter_scaling * accumulator[i][1]);
+        row_out->row[i].x = (int) (filter_scaling * accumulator.row[i].x);
+        row_out->row[i].y = (int) (filter_scaling * accumulator.row[i].y);
     }
 }
 
@@ -195,7 +214,7 @@ static void decimate_block(const struct fa_row *block_in, struct timespec *ts)
     int sample_count_in = fa_block_size / FA_FRAME_SIZE;
     for (int in = 0; in < sample_count_in; in ++)
     {
-        const struct fa_row *row = accumulate(block_in++);
+        const struct fa_row_int64 *row = accumulate(block_in++);
 
         decimation_counter += 1;
         if (decimation_counter >= decimation_factor)
