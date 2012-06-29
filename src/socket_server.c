a34974ecc2d955e5ee6b72126c21b969fd34549a
mga83 <mga83@e099a375-04f9-0310-9d5f-a741eaff62e1>
2012-06-29 10:36:23 +0000

    FA archiver: Implement extended timestamps for subscription data
    For ordinary subscription data we can also provide inline timestamps,
    this simplifies the implementation of fa-capture.  The duration is
    unfortunately not all that accurate, and we don't try to compute this
    for decimated live data; fa-capture doesn't support this anyway.
    
    git-svn-id: svn+ssh://serv0002.cs.diamond.ac.uk/home/subversion/repos/controls/diamond/trunk/python/fa-archiver@79432 e099a375-04f9-0310-9d5f-a741eaff62e1

--- src/socket_server.c
+++ src/socket_server.c
@@ -62,7 +62,7 @@
 
 
 /* String used to report protocol version in response to CV command. */
-#define PROTOCOL_VERSION    "1.0"
+#define PROTOCOL_VERSION    "1.1"
 
 
 /* Block buffer for full resolution FA data. */
@@ -402,44 +402,63 @@ static bool process_command(int scon, const char *client_name, const char *buf)
 }
 
 
+/* Same options as for reader. */
+enum send_timestamp {
+    SEND_NOTHING = 0,               // Don't send timestamp with data
+    SEND_BASIC,                     // Send timestamp at start of data
+    SEND_EXTENDED                   // Send timestamp with each data block
+};
+
 /* Result of parsing a subscribe command. */
 struct subscribe_parse {
     struct filter_mask mask;        // List of BPMs to be subscribed
     struct buffer *buffer;          // Source of data (FA or decimated)
-    bool want_timestamp;            // Set if timestamp should be sent
+    enum send_timestamp send_timestamp; // Timestamp options
     bool want_t0;                   // Set if T0 should be sent
     bool uncork;                    // Set if stream should be uncorked
 };
 
+
+static bool parse_options(const char **string, struct subscribe_parse *parse)
+{
+    parse->send_timestamp =
+        read_char(string, 'T') ?    //        "TE"          "T"             ""
+            read_char(string, 'E') ? SEND_EXTENDED : SEND_BASIC : SEND_NOTHING;
+    parse->want_t0 = read_char(string, 'Z');
+    parse->uncork = read_char(string, 'U');
+    if (read_char(string, 'D'))
+        parse->buffer = decimated_buffer;
+    else
+        parse->buffer = fa_block_buffer;
+    return
+        TEST_NULL_(parse->buffer, "Decimated data not available")  &&
+        TEST_OK_(
+            parse->send_timestamp != SEND_EXTENDED  ||
+            parse->buffer != decimated_buffer,
+            "Extended timestamps not available for decimated data");
+}
+
 /* A subscribe request is a filter mask followed by options:
  *
  *  subscription = "S" filter-mask options
- *  options = [ "T" ] [ "Z" ] [ "U" ] [ "D" ]
+ *  options = [ "T" [ "E" ]] [ "Z" ] [ "U" ] [ "D" ]
  *
  * The options have the following meanings:
  *
  *  T   Start subscription stream with timestamp
+ *  TE  Send extended timestamps
  *  Z   Start subscription stream with t0
  *  U   Uncork data stream
  *  D   Want decimated data stream
  *
- * If both T and Z are specified then the timestamp is sent first before T0.
- */
+ * If both T and Z are specified then the timestamp is sent first before T0. */
 static bool parse_subscription(
     const char **string, struct subscribe_parse *parse)
 {
     return
         parse_char(string, 'S')  &&
         parse_mask(string, &parse->mask)  &&
-        DO_(
-            parse->want_timestamp = read_char(string, 'T');
-            parse->want_t0 = read_char(string, 'Z');
-            parse->uncork = read_char(string, 'U'))  &&
-        IF_ELSE(read_char(string, 'D'),
-            TEST_NULL_(decimated_buffer, "Decimated data not available")  &&
-            DO_(parse->buffer = decimated_buffer),
-            // else D not set
-            DO_(parse->buffer = fa_block_buffer));
+        parse_options(string, parse);
 }
 
 
@@ -464,32 +483,64 @@ bool report_socket_error(int scon, const char *client_name, bool ok)
 }
 
 
+static bool send_extended_header(int scon, size_t block_size)
+{
+    struct extended_timestamp_header header = {
+        .block_size = block_size,
+        .offset = 0 };
+    return TEST_write(scon, &header, sizeof(header));
+}
+
+
+static bool send_extended_timestamp(
+    int scon, size_t block_size, uint64_t timestamp)
+{
+    /* This duration calculation is why we can't do decimated data with extended
+     * timestamps -- just don't have the value to deliver!  Actually, it's not a
+     * great match to the data anyway, but that's another problem... */
+    const struct disk_header *header = get_header();
+    uint32_t duration =
+        header->last_duration / (header->major_sample_count / block_size);
+
+    struct extended_timestamp extended_timestamp = {
+        .timestamp = timestamp - duration,  // timestamp is after *last* point
+        .duration = duration };
+    return TEST_write_(
+        scon, &extended_timestamp, sizeof(extended_timestamp),
+        "Unable to write timestamp");
+}
+
+
 static bool send_subscription(
     int scon, struct reader_state *reader, uint64_t timestamp,
     struct subscribe_parse *parse, const void **block)
 {
     /* The transmitted block optionally begins with the timestamp and T0 values,
      * in that order, if requested. */
+    size_t block_size = reader_block_size(reader) / FA_FRAME_SIZE;
     bool ok =
-        IF_(parse->want_timestamp,
+        IF_(parse->send_timestamp == SEND_BASIC,
             TEST_write(scon, &timestamp, sizeof(uint64_t)))  &&
         IF_(parse->want_t0,
             TEST_write(scon, *block, sizeof(uint32_t)))  &&
         IF_(parse->uncork,
-            set_socket_cork(scon, false));
+            set_socket_cork(scon, false))  &&
+        IF_(parse->send_timestamp == SEND_EXTENDED,
+            send_extended_header(scon, block_size));
 
-    size_t block_size = reader_block_size(reader);
     while (ok)
     {
         ok = FINALLY(
-            write_frames(
-                scon, &parse->mask, *block, block_size / FA_FRAME_SIZE),
+            IF_(parse->send_timestamp == SEND_EXTENDED,
+                send_extended_timestamp(scon, block_size, timestamp))  &&
+            write_frames(scon, &parse->mask, *block, block_size),
+
             // Always do this, even if write_frames fails.
             TEST_OK_(release_read_block(reader),
                 "Write underrun to client"));
         if (ok)
             ok = TEST_NULL_(
-                *block = get_read_block(reader, NULL),
+                *block = get_read_block(reader, &timestamp),
                 "Gap in subscribed data");
         else
             *block = NULL;
