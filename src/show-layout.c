260b2bc8bc1144fa21ef116a76315a9359db5156
mga83 <mga83@e099a375-04f9-0310-9d5f-a741eaff62e1>
2012-09-21 09:16:20 +0000

    FA archiver: Add support for validating external structures
    The interface to the archiver is defined by a number of structures,
    both stored on disk and communicated over ethernet, and it's troublesome
    if any of these change without warning.  This commit adds a framework
    for ensuring that selected structures remain unchanged.  The following
    files are involved:
    
        layout-list
            This lists all structures that need to be tracked.
        layout
            This documents the detailed layout of all structures defined
            in layout-list and can be automatically generated
        make-layout, show-layout.c
            These work together to build layout from layout-list and the
            current headers and compliation settings.
        Makefile
            Generates error if layout needs to be changed.
    
    git-svn-id: svn+ssh://serv0002.cs.diamond.ac.uk/home/subversion/repos/controls/diamond/trunk/python/fa-archiver@83377 e099a375-04f9-0310-9d5f-a741eaff62e1

---
+++ src/show-layout.c
@@ -0,0 +1,58 @@
+/* Code for computing the layout of a given structure.  The following macros
+ * must be defined before compiling this:
+ *  STRUCT_NAME     Name of structure to be analysed
+ *  FIELD_LIST      Name of file containing list of fields in structure
+ *  FIELD_HEADER    Name of file containing definition to include. */
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <inttypes.h>
+#include <string.h>
+
+#include FIELD_HEADER
+
+
+#define S_(x)   #x
+#define S(x)    S_(x)
+
+struct field {
+    const char *name;
+    size_t size;
+    size_t offset;
+};
+
+#define OFFSET(field) \
+    { #field, \
+      sizeof(((struct STRUCT_NAME *) NULL)->field), \
+      offsetof(struct STRUCT_NAME, field) \
+    }
+
+const struct field fields[] = {
+    #include FIELD_LIST
+};
+
+const int field_count = sizeof(fields) / sizeof(fields[0]);
+
+int main(int argc, char **argv)
+{
+    size_t struct_size = sizeof(struct STRUCT_NAME);
+    printf("struct %s: %zu\n", S(STRUCT_NAME), struct_size);
+
+    size_t offset = 0;
+    for (int i = 0; i < field_count; i ++)
+    {
+        const struct field *field = &fields[i];
+        if (offset != field->offset)
+        {
+            printf("    padding: %zu\n", field->offset - offset);
+            offset = field->offset;
+        }
+
+        printf("%-24s: %3zu / %3zu\n", field->name, field->offset, field->size);
+        offset += field->size;
+    }
+    if (offset != struct_size)
+        printf("    padding: %zu\n", struct_size - offset);
+}
