599c187186e83af0330646769e8f0804aef68663
mga83 <mga83@e099a375-04f9-0310-9d5f-a741eaff62e1>
2015-02-13 07:27:36 +0000

    FA archiver: Move progress bar into separate sharable file
    Also improve the display availability test so that we only hide the
    progress bar when running headless, and add hide option.
    
    git-svn-id: svn+ssh://serv0002.cs.diamond.ac.uk/home/subversion/repos/controls/diamond/trunk/python/fa-archiver@127492 e099a375-04f9-0310-9d5f-a741eaff62e1

--- matlab/fa_load.m
+++ matlab/fa_load.m
@@ -486,32 +486,3 @@ function id0 = process_id0( ...
     id0 = reshape(id0', [], 1);
     id0 = id0(initial_offset + 1:initial_offset + sample_count);
 end
-
-
-% Support for cancellable waiting
-function bar = progress_bar(title)
-    function ok = advance_waitbar(bar, fraction)
-        waitbar(fraction, bar.wb);
-        ok = ~getappdata(bar.wb, 'cancelling');
-    end
-
-    function ok = show_advance(fraction)
-        fprintf(2, '%5.2f%%\r', 100 * fraction)
-        ok = true;
-    end
-
-    bar = {};
-    if usejava('desktop')
-        bar.wb = waitbar(0, title, ...
-            'CreateCancelBtn', 'setappdata(gcbf,''cancelling'',1)');
-        bar.cleanup = onCleanup(@() delete(bar.wb));
-        setappdata(bar.wb, 'cancelling', 0);
-
-        bar.advance = @(fraction) advance_waitbar(bar, fraction);
-        bar.cancelled = @() bar.cancelling;
-    else
-        bar.cleanup = onCleanup(@() fprintf(2, 'Done   \n'));
-        bar.advance = @show_advance;
-        bar.cancelled = @() false;
-    end
-end
